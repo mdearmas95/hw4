0fbed2f78c21986a2d89b844d15df85d9df0a467
mdearmas95 <36656417+mdearmas95@users.noreply.github.com>
2018-02-20 12:56:29 -0600

    Add files via upload

---
+++ ComputingHW4part2.m
@@ -0,0 +1,43 @@
+clc;
+clear;
+close all;
+prompt = 'Enter N' %user will input N
+prompt1 = 'Enter k' %user will input k
+N=input(prompt);
+k= input(prompt1);
+h= 1/(N+1);
+A= zeros(N+1,N+1);
+for c = 1:N+1 %for loop to make coeff matrix
+    for r = 1:N+1
+        
+        if r == c
+            A(r,c) = -(2+k^2*h^2);
+        elseif abs(r-c) == 1
+            A(r,c) = 1;
+        else
+            A(r,c) = 0;
+        end
+        
+    end
+end
+A;
+F= zeros(N+1,1); %creates zero matrix for solution
+for d = 1:N+1 %filling in matrix with value using loop and conditional statements
+    if d == 1
+        F(d,1)= h^2+2*h;
+    elseif d== N+1
+        F(N+1,1)= h^2;
+    else 
+        F(d,1)= h^2;
+    end
+end
+F;
+F_1= F(1:N,1); %reducing matrix since u(L)=0
+A_1= A(1:N,1:N); %reducing matrix since u(L)=0
+U= A_1\F_1; %solving for u
+x= h:h:1-h; %creating x vector to plot
+y=(1-(cosh(k*x)/cosh(k)))*(1/k^2) - (1/k)*(sinh(k-k*x))/cosh(k); %given exact function
+p=plot(x,U,'g',x,y,'k') %plotting exact vs approximate value
+p(1).Marker= '*'
+legend('approximate value', 'real value');
+%input N=~ 5120 for k=10 and k=100 to obtain results with low error
